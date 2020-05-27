62e70bf5da758564404a1c3a4cd140aa47d5f7e9
Bryce Mullen <mullen.bryce@gmail.com>
2020-05-26 19:33:13 -0700

    fix fullscreen on safari

--- src/app/components/screen/screen.component.ts
+++ src/app/components/screen/screen.component.ts
@@ -80,6 +80,7 @@ export class ScreenComponent implements OnInit {
   judgeName: string;
   newJudge = true;
   full: boolean;
+  safari: boolean;
 
 
   constructor(
@@ -130,6 +131,9 @@ export class ScreenComponent implements OnInit {
 
     this.isMobile = this.afs.isMobileDevice(navigator.userAgent);
     console.log(`is mobile ${this.isMobile}`);
+    const ua = navigator.userAgent.toLowerCase();
+    this.safari = ua.indexOf('safari') === -1;
+
   }
 
   toggleFullScreen() {
